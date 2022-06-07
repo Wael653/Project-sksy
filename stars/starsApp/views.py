6588ff2f998442118df0d06e16abd1e5e9cd728a
Jan-Ha-He <51442870+Jan-Ha-He@users.noreply.github.com>
2022-06-07 14:00:50 +0200

    Initialized an example view, url and fixed the settings.py

--- stars/starsApp/views.py
+++ stars/starsApp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.shortcuts import render
+from django.forms.models import model_to_dict
+from stars import templates
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
