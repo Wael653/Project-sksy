41652c7d80215297ef6733951648411157e4bb34
Wael653 <104505577+Wael653@users.noreply.github.com>
2022-06-09 20:24:14 +0200

    import models to view.py

--- stars/starsApp/views.py
+++ stars/starsApp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from django.shortcuts import render
 from django.forms.models import model_to_dict
+from .models import Workplace
+from .models import User
 
 # Create your views here.
 def index(request):
