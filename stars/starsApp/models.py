b43b1a1890d25ee30f0f090b7c5d2d6293709fd8
Wael653 <104505577+Wael653@users.noreply.github.com>
2022-06-09 20:21:33 +0200

    add models

--- stars/starsApp/models.py
+++ stars/starsApp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+# Create your models here.
+
+
+class Workplace(models.Model):
+    wp_id = models.PositiveIntegerField(primary_key=True)
+    number = models.IntegerField()
+    reserved = models.BooleanField(default = False)
+    def __str__(self):
+        return self.number
+
+class User(models.Model):
+    place = models.OneToOneField( Workplace, on_delete = models.CASCADE)
+    name = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    user_id = models.PositiveIntegerField(primary_key=True)
+    def __str__(self):
+        return self.name
 
 # Create your models here.
