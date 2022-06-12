ecd0f53e2101f9406a60c03577c0c0fcad010e29
Wael653 <104505577+Wael653@users.noreply.github.com>
2022-06-12 21:17:59 +0200

    add Contact to admin interface

--- stars/starsApp/admin.py
+++ stars/starsApp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Workplace
+from .models import Contact
 
 # Register your models here.
-admin.site.register(Workplace)
+admin.site.register(Contact)
