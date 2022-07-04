bdc76a4099079abb6099ba949b22e4c0e3c9eba2
Jan-Ha-He <51442870+Jan-Ha-He@users.noreply.github.com>
2022-07-04 23:46:28 +0200

    Minor changes to the modeladded initialData.json

--- stars/starsApp/admin.py
+++ stars/starsApp/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Workplace
+from .models import Unit, Room, RoomDevice, Workplace, WorkplaceDevice
 
 # Register your models here.
-
 admin.site.register(Workplace)
+admin.site.register(Unit)
+admin.site.register(Room)
+admin.site.register(RoomDevice)
+admin.site.register(WorkplaceDevice)
